19350da94610ef89a40227d0e68637609a66e84d
Sigi1013 <188875106+Sigi1013@users.noreply.github.com>
2025-05-04 20:29:20 +0200

    Add removeMovieWatchedList method to MovieManager

--- src/main/java/MovieManager.java
+++ src/main/java/MovieManager.java
@@ -9,9 +9,7 @@ public class MovieManager {
     }
 
     public static void getWatchList(){
-        System.out.println("-------------------------");
-        System.out.println("Watched Movies List.");
-        System.out.println("-------------------------");
+        System.out.println("\nWatched Movies List.");
         for(Movie movie : WatchList){
             System.out.println("\nTitle: " + movie.getTitle() + " | "
                     + "Released: " + movie.getYear() + " | "
@@ -20,25 +18,39 @@ public class MovieManager {
                     + "Imdb rating: " + movie.getImdbRating() + " | "
             );
         }
-        System.out.println("-------------------------");
     }
 
     public static void addToWatchedMoviesList(Movie movie){
         WatchedMoviesList.add(movie);
     }
 
-    public static void getWatchedMoviesList(){
-        System.out.println("-------------------------");
-        System.out.println("Watched Movies List.");
-        System.out.println("-------------------------");
-        for(Movie movie : WatchedMoviesList){
-            System.out.println("Title: " + movie.getTitle() + " | "
-                    + "Released: " + movie.getYear() + " | "
-                    + "Runtime: " + movie.getRuntime() + " | "
-                    + "Director: " + movie.getDirector() + " | "
-                    + "Imdb rating: " + movie.getImdbRating() + " | "
-            );
+    public static boolean getWatchedMoviesList(){
+        if(WatchedMoviesList.isEmpty()){
+
+            System.out.println("This list is empty. \n" +
+                    "To add a movie, search for it use the number 1. option in main menu.");
+
+            return false;
+        }else{
+            int index = 0;
+
+            System.out.println("\nWatched Movies List.");
+
+            for(Movie movie : WatchedMoviesList){
+                System.out.println("ID:" + index +" | Title: " + movie.getTitle() + " | "
+                        + "Released: " + movie.getYear() + " | "
+                        + "Runtime: " + movie.getRuntime() + " | "
+                        + "Director: " + movie.getDirector() + " | "
+                        + "Imdb rating: " + movie.getImdbRating() + " | "
+                );
+                index++;
+            }
+
+            }
+        return true;
         }
-        System.out.println("-------------------------");
+
+    public static void removeMovieWatchedList(int index){
+        WatchedMoviesList.remove(index);
     }
 }
