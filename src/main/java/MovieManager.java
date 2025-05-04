29807b61d8d2fb562981978094eab06f3c24185d
Sigi1013 <188875106+Sigi1013@users.noreply.github.com>
2025-05-04 16:54:54 +0200

    Add methods addToWatchList and getWatchList to MovieManager

--- src/main/java/MovieManager.java
+++ src/main/java/MovieManager.java
@@ -1,5 +1,26 @@
 import java.util.ArrayList;
 
 public class MovieManager {
+    static private final ArrayList<Movie> WatchList = new ArrayList<>();
 
+    public static void addToWatchList(Movie movie){
+        WatchList.add(movie);
+    }
+
+    public static void getWatchList(){
+        System.out.println("-------------------------");
+        System.out.println("Watched Movies List.");
+        System.out.println("-------------------------");
+        for(Movie movie : WatchList){
+            System.out.println("\nTitle: " + movie.getTitle() + " | "
+                    + "Released: " + movie.getYear() + " | "
+                    + "Runtime: " + movie.getRuntime() + " | "
+                    + "Director: " + movie.getDirector() + " | "
+                    + "Imdb rating: " + movie.getImdbRating() + " | "
+            );
+        }
+        System.out.println("-------------------------");
+    }
+
+    
 }
